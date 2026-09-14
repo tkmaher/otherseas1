8b836dd8eda1e8bd05fdb1aa667d7ad55a981d87
tkmaher <tkmaher@umich.edu>
2026-09-13 22:04:02 -0500

    suspenseful

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
 import data from "@/data/items.json";
 import Main from "@/components/main";
+import { Suspense } from "react";
 
 export default function Home() {
 
   return (
-    <Main data={data}/>
+    <Suspense fallback={<div style={{margin: "auto"}}>otherseas1.com: loading...</div>}>
+      <Main data={data}/>
+    </Suspense>
   );
 }
